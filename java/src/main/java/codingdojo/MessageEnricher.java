aa70adf83da221a4430142bc47dd2740c1411903
Samuel Ytterbrink <say@praqma.net>
2019-05-21 16:19:20 +0200

    !!! replace usage of message field call to getter.

--- java/src/main/java/codingdojo/MessageEnricher.java
+++ java/src/main/java/codingdojo/MessageEnricher.java
@@ -66,7 +66,7 @@ public class MessageEnricher {
     public String toString() {
         return "ErrorResult{" +
                 "formulaName='" + formulaName + '\'' +
-                ", message='" + message + '\'' +
+                ", message='" + getMessage() + '\'' +
                 ", presentation='" + presentation + '\'' +
                 '}';
     }
