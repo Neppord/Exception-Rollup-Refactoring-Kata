0da7855b5784f1f376ffacbd3dfcd1acf7a8e3f0
Samuel Ytterbrink <say@praqma.net>
2019-05-21 16:49:27 +0200

    r add field

--- java/src/main/java/codingdojo/Normal.java
+++ java/src/main/java/codingdojo/Normal.java
@@ -1,12 +1,16 @@
 package codingdojo;
 
 public class Normal extends ErrorResult {
+
+    private final String message;
+
     protected Normal(Exception exception, String formulaName, String presentation) {
         super(exception, formulaName, presentation);
+        message = exception.getMessage();
     }
 
     @Override
     public String getMessage() {
-        return exception.getMessage();
+        return message;
     }
 }
